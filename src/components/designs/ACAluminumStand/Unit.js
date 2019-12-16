e12b4eccf2dab7d9830ca325a1f0d759ad005be3
acuba001 <4492431+acuba001@users.noreply.github.com>
2019-12-15 21:21:12 -0500

    Added unit-wise validation.

--- src/components/designs/ACAluminumStand/Unit.js
+++ src/components/designs/ACAluminumStand/Unit.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import classnames from 'classnames'
 
 export default class Unit extends Component {
 
@@ -6,7 +7,11 @@ export default class Unit extends Component {
     width: "", 
     deep: "", 
     high: "", 
-    weight: ""
+    weight: "",
+    topAreaGood: true, 
+    frontAreaGood: true, 
+    highGood: true, 
+    weightGood: true
   }
 
   RegExp = new RegExp(/^\d*\.?\d*$/)
@@ -15,22 +20,79 @@ export default class Unit extends Component {
 
   onChange = (e) => {
     if(this.isValidNum(e.target.value)){
-      this.setState({...this.state, [e.target.name]: e.target.value})
+
+      const { width, deep, high} = this.state
+
+      if(e.target.name === "weight"){
+        this.setState({
+          ...this.state, 
+          weight: e.target.value,
+          weightGood: e.target.value === "" || parseFloat(e.target.value) < 450
+        })
+      }
+      else if (e.target.name === "high"){
+        this.setState({
+          ...this.state, 
+          high: e.target.value,
+          highGood: e.target.value === "" || parseFloat(e.target.value) <= 60,
+          frontAreaGood: this.area(width, e.target.value) < 50
+        })
+      } else if(e.target.name === "width"){
+        this.setState({
+          ...this.state, 
+          width: e.target.value,
+          topAreaGood: this.area(e.target.value, deep) <= 37.9,
+          frontAreaGood: this.area(e.target.value, high) < 50
+        })
+      } else if(e.target.name === "deep"){
+        this.setState({
+          ...this.state, 
+          deep: e.target.value,
+          topAreaGood: this.area(width, e.target.value) <= 37.9
+        })
+      }
     }
+  }
+
+  area = (x, y) => {
+    if(x === "" || y === "") return 0
+    return parseFloat(x) * parseFloat(y) / 144.0
   }
 
   render() {
 
     const { ind } = this.props
-    const { width, deep, high, weight } = this.state
+    const { 
+      width, 
+      deep, 
+      high, 
+      weight,
+      topAreaGood,
+      frontAreaGood,
+      highGood,
+      weightGood
+    } = this.state
 
     return (
       <tr>
         <th scope="row">#{ind}</th>
-        <td><input className="form-control" type="text" name={"width"} value={width} onChange={this.onChange} placeholder="Width" /></td>
-        <td><input className="form-control" type="text" name={"deep"} value={deep} onChange={this.onChange} placeholder="Deep" /></td>
-        <td><input className="form-control" type="text" name={"high"} value={high} onChange={this.onChange} placeholder="High" /></td>
-        <td><input className="form-control" type="text" name={"weight"} value={weight} onChange={this.onChange} placeholder="Weight" /></td>
+        <td>
+          <input className={classnames({'form-control': true, 'is-invalid': !topAreaGood || !frontAreaGood})} type="text" name={"width"} value={width} onChange={this.onChange} placeholder="Width" />
+          { !topAreaGood ? (<div className="invalid-feedback"> Top area of unit too high!</div>) : null }
+          { !frontAreaGood ? (<div className="invalid-feedback"> Front area of unit too high!</div>) : null }
+        </td>
+        <td>
+          <input className={classnames({'form-control': true, 'is-invalid': !topAreaGood})} type="text" name={"deep"} value={deep} onChange={this.onChange} placeholder="Deep" />
+          {  }
+        </td>
+        <td>
+          <input className={classnames({'form-control': true, 'is-invalid': !highGood || !frontAreaGood})} type="text" name={"high"} value={high} onChange={this.onChange} placeholder="High" />
+          { !highGood ? (<div className="invalid-feedback"> Unit too high!</div>) : null }
+        </td>
+        <td>
+          <input className={classnames({'form-control': true, 'is-invalid': !weightGood})} type="text" name={"weight"} value={weight} onChange={this.onChange} placeholder="Weight" />
+          { !weightGood ? (<div className="invalid-feedback"> Weight of unit too high!</div>) : null }
+        </td>
       </tr>
     )
   }
